b3cf078b44900a2ae796c004ab1d633f564cea54
nguyenyou <tunt081295@gmail.com>
2025-12-24 01:05:54 +0700

    simplify

--- mill-build/src/LibraryBaseModule.scala
+++ mill-build/src/LibraryBaseModule.scala
@@ -1,11 +1,16 @@
 package millbuild
 
 import mill.javalib.*
+import mill.javalib.publish.*
+import mill.scalajslib.*
+import mill.scalajslib.api.*
 import mill.scalalib.*
 
-trait LibraryBaseModule extends ScalaModule with SbtModule {
+trait LibraryBaseModule extends ScalaJSModule with SbtModule {
 
   def scalaVersion = "3.3.4"
+
+  def scalaJSVersion = "1.20.1"
 
   def scalacOptions = super.scalacOptions() ++ Seq(
     "-deprecation",
